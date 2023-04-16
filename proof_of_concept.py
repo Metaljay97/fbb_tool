d14d0dd09f13f0d3d156ad21c19938244b61dcc9
Jaden <u0982594@utah.edu>
2023-04-15 22:58:04 -0600

    first push, really basic POC stuff

---
+++ proof_of_concept.py
@@ -0,0 +1,13 @@
+from nba_api.stats.library.parameters import LeagueID, Season, SeasonType, StatCategory
+from nba_api.stats.endpoints import leaguedashplayerstats
+
+season = "2020-21"  # Replace with the season you're interested in, e.g. '2019-20'
+stats = leaguedashplayerstats.LeagueDashPlayerStats(
+    season=season,
+    season_type_all_star="Regular Season",
+    league_id_nullable=LeagueID.default,
+    per_mode_detailed="PerGame",
+)
+
+data = stats.get_data_frames()[0]  # Get the first (and only) DataFrame
+print(data)  # Print the first five rows of the DataFrame
