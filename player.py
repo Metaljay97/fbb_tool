e9723206eb5cf98884bcde58d4cb5c9628eb7ce8
Jaden <u0982594@utah.edu>
2023-04-21 17:18:55 -0600

    add super basic player file

---
+++ player.py
@@ -0,0 +1,20 @@
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.endpoints import commonallplayers
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        player_info = players.find_players_by_full_name(name)
+        self.id = player_info[0]["id"]
+
+    def get_game_log_for_year(self, year):
+        # Get player game log for given year
+        return playergamelog.PlayerGameLog(player_id=self.id, season=year)
+
+
+# Create an instance of the class
+my_instance = Player("James Harden")
+
+print(my_instance.get_game_log_for_year("2022").get_data_frames()[0])
